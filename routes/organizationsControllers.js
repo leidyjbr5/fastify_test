f1ee9a29d98dba53c34a43c5229b387db92da4d0
Leidy Bohorquez <leidy@netuxtecnologia.com>
2022-02-16 11:41:02 -0500

    test upload large files

--- routes/organizationsControllers.js
+++ routes/organizationsControllers.js
@@ -39,6 +39,11 @@ async function createController(request, reply) {
       ContentType: data.mimetype,
     }),
   )
+
+  // const used = process.memoryUsage();
+  // for (let key in used) {
+  //   console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
+  // }
   const organization = await createOrganizations({
     name: data.fields.name.value,
     logoName: data.fieldname,
